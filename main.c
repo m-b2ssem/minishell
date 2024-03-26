021d36bac08dd34667cd9e1c780d09979bfe695b
Basem <basemmahdi@gmail.com>
2024-03-26 16:48:30 +0100

    bringing some stuff from the last commit

--- main.c
+++ main.c
@@ -1,51 +1,20 @@
-int main()
+#include "minishell.h"
+
+void    execute(char *cml)
 {
+    redirections(cml);
+}
+
+int main(int argc,char *argv[], char *env[])
+{
+    (void)argc;
+    (void)argv;
+    (void)env;
+    while (1)
+    {
+        char *input = readline("minishell> ");
+        execute(input);
+    }
+
     return 0;
-}
-
-int exec(char **argv, char **envp, int i) 
-{
-    int fd[2];
-    int status;
-    int has_pipe = argv[i] && !strcmp(argv[i], "|");
-
-    if (has_pipe && pipe(fd) == -1)
-        return err("error: fatal\n");
-
-    int pid = fork();
-    if (!pid) 
-    {
-        argv[i] = 0;
-        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-            return err("error: fatal\n");
-        execve(*argv, argv, envp);
-        return err("error: cannot execute "), err(*argv), err("\n");
-    }
-
-    waitpid(pid, &status, 0);
-    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-        return err("error: fatal\n");
-    return WIFEXITED(status) && WEXITSTATUS(status);
-}
-
-int main(int argc, char **argv, char **envp) 
-{
-    int    i = 0;
-    int    status = 0;
-
-    if (argc > 1) 
-    {
-        while (argv[i] && argv[++i]) 
-        {
-            argv += i;
-            i = 0;
-            while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-                i++;
-            if (!strcmp(*argv, "cd"))
-                status = cd(argv, i);
-            else if (i)
-                status = exec(argv, envp, i);
-        }
-    }
-    return status;
 }
