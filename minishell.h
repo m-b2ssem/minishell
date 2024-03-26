e25b955495dcabbd88773134236b39d290cb6e34
Basem <basemmahdi@gmail.com>
2024-03-26 16:37:00 +0100

    no need

--- minishell.h
+++ minishell.h
@@ -1,10 +1,11 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+# include "libft/libft.h"
 # include <signal.h>
 # include <stdio.h>
 # include <string.h>
+//# include <sys/wait.h>
 
 
 #endif
