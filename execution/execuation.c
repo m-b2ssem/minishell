937f50645ee5bbc207f30cbdf5836bad3cbb13db
Bassem MAHDI <bassem@Bassems-MacBook-Pro.local>
2024-05-19 10:28:42 +0200

    going back

--- execution/execuation.c
+++ execution/execuation.c
@@ -6,15 +6,17 @@ void	handle_non_builtin(t_cmd *cmd, pid_t *pross_id, t_cmd *tmp)
 {
 	struct stat	file_stat;
 	char		**new_env;
-
-
+ 
 	cmd->path = get_path(cmd->arg_arr[0], cmd->env);
 	if (cmd->path == NULL)
+	{
 		clean_exit(tmp, pross_id, 127);
+	}
 	new_env = env_to_char(cmd->env);
 	if (new_env == NULL)
 		clean_exit(tmp, pross_id, 127);
 	execve(cmd->path, cmd->arg_arr, new_env);
+	printf("here\n");
 	if (stat(cmd->arg_arr[0], &file_stat) == 0)
 	{
 		ft_putstr_fd("minishell: ", 2);
