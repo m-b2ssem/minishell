78b0190f8092db47a83c00afe20ac88c290b174e
Bassem MAHDI <bassem@Bassems-MacBook-Pro.local>
2024-05-19 10:31:06 +0200

    going back

--- execution/signals.c
+++ execution/signals.c
@@ -15,7 +15,7 @@ void    signal_handler(int signum)
         g_signal = 130;
         write(STDOUT_FILENO, "\n", 1);
         rl_on_new_line();
-        //rl_replace_line("", 0);
+        rl_replace_line("", 0);
         rl_redisplay();
     }
 }
