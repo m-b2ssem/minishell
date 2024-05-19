4eb0bf1843dec74304997d78d6bc035fa468c215
Basem Mahdi <bmahdi@c3r8p5.42vienna.com>
2024-05-19 14:16:16 +0200

    fix the red <, >> and > but not <<

--- redirections/redirections.c
+++ redirections/redirections.c
@@ -6,7 +6,7 @@ static int redirections_in(t_cmd *cmd, t_token *token)
     {
         if (cmd->fd_in != 0)
             close(cmd->fd_in);
-        cmd->fd_in = open(token->string, O_RDONLY);
+        cmd->fd_in = open(token->next->string, O_RDONLY);
         if (cmd->fd_in == -1)
             return (-1);
     }
@@ -14,7 +14,7 @@ static int redirections_in(t_cmd *cmd, t_token *token)
     {
         if (cmd->fd_in != 0)
             close(cmd->fd_in);
-        cmd->fd_in = heredoc(cmd, token->string);
+        cmd->fd_in = heredoc(cmd, token->next->string);
         if (cmd->fd_in == -1)
             return (-1);
     }
@@ -36,7 +36,7 @@ static int redirections_out(t_cmd *cmd, t_token *token)
     {
         if (cmd->fd_out != 1)
             close(cmd->fd_out);
-        cmd->fd_out = open(token->string, O_CREAT | O_RDWR | O_APPEND, 0644);
+        cmd->fd_out = open(token->next->string, O_CREAT | O_RDWR | O_APPEND, 0644);
         if (cmd->fd_out == -1)
             return (-1);
     }
@@ -54,12 +54,12 @@ int redirections(t_cmd *cmd)
         if (tmp->type == REDIR_IN || tmp->type == DELIM)
         {
             if (redirections_in(cmd, tmp))
-                return (-1);
+                return (1);
         }
         if (tmp->type == REDIR_OUT || tmp->type == APPEND)
         {
             if (redirections_out(cmd, tmp))
-                return (-1);
+                return (1);
         }
         tmp = tmp->next;
     }
